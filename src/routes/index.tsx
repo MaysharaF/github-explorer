91c75893ea6f7f6b2b1bfe66dfb284e67d51a48d
MaysharaF <mayshara@alunos.utfpr.edu.br>
2021-03-15 18:04:46 -0300

    Adicionando rota da page de repository

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,8 @@ import Repository from "../pages/Repository";
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" component={Dashboard} />
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repository} />
   </Switch>
 );
 

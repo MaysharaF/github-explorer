735dc770f428d2508cd110b02cba2e59a87c1619
MaysharaF <mayshara@alunos.utfpr.edu.br>
2021-03-15 18:37:01 -0300

    Criando estilização global

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+
+import GlobalStyles from "./styles/global";
 import Routes from "./routes";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes />;
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+      <GlobalStyles />
+    </>
   );
 };
 
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 
-// import { Container } from './styles';
-
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return <h1>Explore repositórios no Github</h1>;
 };
 
 export default Dashboard;

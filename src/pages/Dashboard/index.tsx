d261b415b27e29b3e854fcc323bae8e90a8af8c2
MaysharaF <mayshara@alunos.utfpr.edu.br>
2021-03-16 21:53:10 -0300

    Criando layout e card da page dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,35 @@
 import React from "react";
 
+import logoImg from "../../assets/logo.svg";
+
+import { Title, Form, Repositories } from "./styles";
+
 const Dashboard: React.FC = () => {
-  return <h1>Explore repositórios no Github</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explore" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="">
+          <img
+            src="https://avatars.githubusercontent.com/u/43793682?s=460&u=b14dc2c3df027bc38f5cf747427bfe6ddd9875cb&v=4"
+            alt="Mayshara Fernandes"
+          />
+
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
